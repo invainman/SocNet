2d12a3ff1ebb3eb53833daffe6c66d9a9549ee7e
ramil.abilov <savekennedy@mail.ru>
2022-04-01 01:51:32 +0300

    changed Singleton

--- src/main/java/com/box/SocNet/singleton/Config.java
+++ src/main/java/com/box/SocNet/singleton/Config.java
@@ -6,17 +6,28 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.Date;
 
+
 @Configuration
 public class Config {
 
     @Autowired
-    private Notification timeNotification;
+    private Notification timeNotification1;
+
+    @Autowired
+    private Notification timeNotification2;
 
     @Bean
-    public void testSingleton(){
+    public void testSingleton() throws InterruptedException {
         Date date = new Date();
-        timeNotification.setDate(date);
-        System.out.println(timeNotification.getDate());
+        timeNotification1.setDate(date);
+        System.out.println("server started in " + timeNotification1.getDate());
+        System.out.println("server started in " + timeNotification2.getDate());
 
+        Thread.sleep(5000);
+
+        Date date2 = new Date();
+        timeNotification2.setDate(date2);
+        System.out.println("server started in " + timeNotification1.getDate() + " (not right time)");
+        System.out.println("server started in " + timeNotification2.getDate() + " (not right time)");
     }
 }
